b82b9dc90a08ddf443763e16fe5acef02955a172
zjm <2215539769@qq.com>
2023-08-02 17:28:26 +0800

    测试v-model绑定组件

--- Vue/05vue3/vue3-pinia/src/App.vue
+++ Vue/05vue3/vue3-pinia/src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import SonCom1 from './components/SonCom1.vue';
 import SonCom2 from './components/SonCom2.vue';
+import SonCom3 from './components/SonCom3.vue';
 import { useUserStore } from '@/store/user.js'
+import {ref} from 'vue';
 const store = useUserStore()
 import { useCountStore } from '@/store/count.js'
 const CountStore=useCountStore()
 import { useChannelStore } from '@/store/channel.js'
 const ChannelStore=useChannelStore()
+const data =ref('')
 
 </script>
 <template>
@@ -19,5 +22,9 @@ const ChannelStore=useChannelStore()
     <ul>
         <li v-for="item in ChannelStore.list" :key="item.id">{{item.name}}</li>
     </ul>
+
+    <!-- 放置一个子组件 -->
+      {{data}}
+    <SonCom3 v-model="data"></SonCom3>
 </template>
 <style scoped></style>
---
+++ Vue/05vue3/vue3-pinia/src/components/SonCom3.vue
@@ -0,0 +1,25 @@
+<script setup>
+// import {ref} from 'vue'
+defineProps({
+    modelValue:String
+})
+const emit =defineEmits(['update:modelValue'])
+const add =()=>{
+    emit('update:modelValue','吉萨汇顶科技花洒客户端')
+}
+// const changeInfo =(e)=>{
+//     console.log(e);
+//     emit('update:modelValue',e.target.value)
+// }
+</script>
+<template>
+    {{modelValue}}
+<!-- <input type="text" :value="modelValue" @input="changeInfo($event)" > -->
+<select :value="modelValue" @change="emit('update:modelValue', $event.target.value)">
+    <option value='1' label="第一">11</option>
+    <option value='2' label="第二">22</option>
+    <option value='3' label="第三">33</option>
+</select>
+    <button @click="add" >点击改变值</button>
+</template>
+<style scoped></style>
