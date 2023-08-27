94a1edd819654ec98d149c562b6a7ac790720df6
zjm <2215539769@qq.com>
2023-08-27 17:01:46 +0800

    小程序组件属性数据监听

--- 微信小程序/mp_04/components/demo/demo.js
+++ 微信小程序/mp_04/components/demo/demo.js
@@ -7,6 +7,11 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    max:Number,
+    min:{
+      type:Number, // 数据类型
+      value:10   //设置默认值
+    }
 
   },
 
@@ -14,6 +19,10 @@ Component({
    * 组件的初始数据
    */
   data: {
+    count:0,
+    n1:0,
+    n2:0,
+    sum:0
 
   },
 
@@ -21,6 +30,24 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    fn(){
+      if(this.data.count>=this.properties.max) return
+      this.setData({
+        count:this.data.count+1
+      })
+      this._mes()
+    },
+    _mes(){
+      wx.showToast({
+        title: 'count是:'+this.data.count,
+        icon:'none'
+      })
+    }
 
+  },
+  observers:{
+    'n1,n2':function(newN1,newN2){
+      
+    }
   }
 })
---
+++ 微信小程序/mp_04/components/demo2/demo2.js
@@ -0,0 +1,61 @@
+// components/demo2/demo2.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  options:{
+    pureDataPattern:/^_/
+  },
+  data: {
+    rgb: {
+      r: 0,
+      g: 0,
+      b: 0
+    },
+    fullColor: '0,0,0'
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    changeR() {
+      this.setData({
+        'rgb.r': this.data.rgb.r + 5 > 255 ? 255 : this.data.rgb.r + 5
+      })
+    },
+    changeG() {
+      this.setData({
+        'rgb.g': this.data.rgb.g + 5 > 255 ? 255 : this.data.rgb.g + 5
+      })
+
+    },
+    changeB() {
+      this.setData({
+        'rgb.b': this.data.rgb.b + 5 > 255 ? 255 : this.data.rgb.b + 5
+      })
+    },
+
+  },
+  observers:{
+    //监听rgb
+    // 'rgb.r,rgb.g,rgb.b':function(newR,newG,newB){
+    //   this.setData({
+    //     fullColor:`${newR},${newG},${newB}`
+    //   })
+    // },
+    'rgb.**':function(obj){
+      this.setData({
+        fullColor:`${obj.r},${obj.g},${obj.b}`
+      })
+    }
+  }
+})
