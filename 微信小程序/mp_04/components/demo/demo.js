82eaf89bd9ede4a8f4b9b12e84e7d1f03c9497fc
zjm <2215539769@qq.com>
2023-08-27 15:14:46 +0800

    小程序组件的基本学习

---
+++ 微信小程序/mp_04/components/demo/demo.js
@@ -0,0 +1,26 @@
+// components/demo/demo.js
+Component({
+  options:{
+    styleIsolation:"shared"
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
