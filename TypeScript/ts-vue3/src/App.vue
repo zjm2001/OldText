c124c855102aba36a1255f5cdf9404ed136e858b
zjm <2215539769@qq.com>
2023-09-20 20:45:13 +0800

    vue中的ts

---
+++ TypeScript/ts-vue3/src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import Son from '@/components/Son.vue'
+import { ref } from 'vue';
+const house=ref('大别野')
+const age =ref(5555)
+//子传递到父自定义函数触发事件
+const fn =(val:string)=>{
+    console.log(val);
+    
+}
+//
+const fn1 =(val:string)=>{
+    console.log(val);
+    
+}
+
+
+//ref在ts中
+const msg =ref('statad')
+const mag1=ref<string>('大师的撒')
+// 或者是复杂类型
+const list =ref([
+    {id:1,name:'达大厦',fid:false},
+    {id:2,name:'达大55厦',fid:false},
+    {id:3,name:'达大46厦',fid:true},
+]) 
+//(这样写就会有各种代码提示)
+type ToDo ={
+    id: number;
+    name: string;
+    fid: boolean;
+}[]
+const list1=ref<ToDo>([
+    {id:1,name:'达大厦',fid:false},
+    {id:2,name:'达大55厦',fid:false},
+    {id:3,name:'达大46厦',fid:true},
+])
+
+list1.value[0].fid
+</script>
+
+<template>
+我是app
+<Son :house="house" :age="age" @getGift="fn" @getGift1="fn1"></Son>
+<Son :house="house" ></Son>
+<h1>----------------------------------------------------------</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ TypeScript/ts-vue3/src/components/Son.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+//非ts语法
+// defineProps({
+//     house:{
+//         type:String,
+//         required:true
+//     }
+// })
+
+//TS语法
+type TypeProps={
+    house:string,  //必须传递
+    age?:number   //可选参数
+}
+//设置默认值 (值必须是可选参数,当不传值则为默认值) 
+ const props=withDefaults( defineProps<TypeProps>(),{
+    age:888  //设置不传值的默认值
+ })
+
+
+
+ //子传父
+ //传统写法
+// const emit=defineEmits(['getGift'])
+// emit('getGift','孙子')
+
+
+// ts写法
+//泛型对象中有几个事假就写几个约束
+// ():中有n个参数 e是固定的其他就按照具体来传递参数
+//语法 (e:事件名,参数名1:类型,参数名2:类型...)
+const emit =defineEmits<{
+(e:'getGift',gift:string):void,
+(e:'getGift1',gift:string):void
+}>()
+emit('getGift','孙子')
+emit('getGift1','孙子5464')
+
+</script>
+
+<template>
+    <h3>
+        我是son
+        直接父亲传递:{{house}}
+        我是可选参数大小:{{age}}平方米
+    </h3>
+
+</template>
+
+<style scoped>
+
+</style>
