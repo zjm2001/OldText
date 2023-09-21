1740739cb78e8f4b1b2312b53d89004429bbc9a1
zjm <2215539769@qq.com>
2023-09-21 20:23:47 +0800

    ts类型声明文件

--- TypeScript/ts-vue3/src/App.vue
+++ TypeScript/ts-vue3/src/App.vue
@@ -1,51 +1,82 @@
 <script setup lang="ts">
 import Son from '@/components/Son.vue'
-import { ref } from 'vue';
-const house=ref('大别野')
-const age =ref(5555)
+import { ref, computed } from 'vue';
+const house = ref('大别野')
+const age = ref(5555)
 //子传递到父自定义函数触发事件
-const fn =(val:string)=>{
+const fn = (val: string) => {
     console.log(val);
-    
+
 }
 //
-const fn1 =(val:string)=>{
+const fn1 = (val: string) => {
     console.log(val);
-    
+
 }
 
 
 //ref在ts中
-const msg =ref('statad')
-const mag1=ref<string>('大师的撒')
+const msg = ref('statad')
+const mag1 = ref<string>('大师的撒')
 // 或者是复杂类型
-const list =ref([
-    {id:1,name:'达大厦',fid:false},
-    {id:2,name:'达大55厦',fid:false},
-    {id:3,name:'达大46厦',fid:true},
-]) 
+const list = ref([
+    { id: 1, name: '达大厦', fid: false },
+    { id: 2, name: '达大55厦', fid: false },
+    { id: 3, name: '达大46厦', fid: true },
+])
 //(这样写就会有各种代码提示)
-type ToDo ={
+type ToDo = {
     id: number;
     name: string;
     fid: boolean;
 }[]
-const list1=ref<ToDo>([
-    {id:1,name:'达大厦',fid:false},
-    {id:2,name:'达大55厦',fid:false},
-    {id:3,name:'达大46厦',fid:true},
+const list1 = ref<ToDo>([
+    { id: 1, name: '达大厦', fid: false },
+    { id: 2, name: '达大55厦', fid: false },
+    { id: 3, name: '达大46厦', fid: true },
 ])
 
 list1.value[0].fid
+
+//计算属性 ts中使用 绝大部分常见都是不用指定类型,会类型推导
+
+const letCount = computed<number>(() => {
+    return list.value.filter(item => !item.fid).length
+})
+
+// 事件类型ts中
+const fnn = (e: MouseEvent) => {
+    console.log(e.pageX, e.pageY);
+
+
+}
+
+/////获取dom对象的ref
+const img = document.createElement('img')   //来判断图片是什么类型
+const gx = ref<HTMLImageElement | null>(null)
+const btn = () => {
+    console.log(gx.value);
+    if (gx.value) {  //防止不存在
+        //需求当点击后修改图片的地址直接写会报错应该指定类型
+        gx.value.src = 'https://img2.baidu.com/it/u=3853345508,384760633&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=1200'
+    }
+
+}
+
 </script>
 
 <template>
-我是app
-<Son :house="house" :age="age" @getGift="fn" @getGift1="fn1"></Son>
-<Son :house="house" ></Son>
-<h1>----------------------------------------------------------</h1>
+    我是app
+    <Son :house="house" :age="age" @getGift="fn" @getGift1="fn1"></Son>
+    <Son :house="house"></Son>
+    <h1>----------------------------------------------------------</h1>
+    {{ letCount }}
+    <h1>----------------------------------------------------------</h1>
+    <button @click="fnn">点我获取位置</button>
+    <h1>------------------------------------------------------</h1>
+    <img ref="gx" src="https://img2.baidu.com/it/u=2969114901,1558934322&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=529" alt="">
+
+    <button @click="btn">获取图片dom</button>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
